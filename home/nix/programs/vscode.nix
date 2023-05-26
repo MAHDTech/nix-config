6c808e77569308be421fae7a1077885aabdda7ff
MAHDTech <MAHDTech@saltlabs.tech>
2023-05-27 00:21:54 +1000

    fix: stuff

--- home/nix/programs/vscode.nix
+++ home/nix/programs/vscode.nix
@@ -1,10 +1,15 @@
-let
+{
+  inputs,
+  pkgs,
+  ...
+}: let
   pkgsUnstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
 
   unstablePkgs = with pkgsUnstable; [];
 
   # Visual Studio Code Insiders
-  vscode-insiders = (pkgsUnstable.vscode.override {isInsiders = true;}).overrideAttrs rec {
+  #vscode-insiders = (pkgs.vscode.override {isInsiders = true;}).overrideAttrs (oldAttrs: rec {
+  vscode-insiders = (pkgs.vscode.override {isInsiders = true;}).overrideAttrs rec {
     src = builtins.fetchTarball {
       url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
       sha256 = "sha256:1nvmnf4w2894v21zcmh1xzcxzzilc10qsqhz2i5hqvrn2vcw0ivv";
