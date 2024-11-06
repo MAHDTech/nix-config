b611ea87472d62dbc7b8ee9c824de49691ac1685
MAHDTech <MAHDTech@saltlabs.tech>
2024-11-07 08:56:21 +1100

    fix: Bluetooth audio

--- nixos/system/config/audio/default.nix
+++ nixos/system/config/audio/default.nix
@@ -16,15 +16,24 @@
     systemWide = true;
     socketActivation = true;
 
-    audio = {enable = true;};
-
     wireplumber = {
       enable = true;
       package = pkgs.wireplumber;
+      extraConfig = {
+        bluetoothEnhancements = {
+          "monitor.bluez.properties" = {
+            "bluez5.enable-sbc-xq" = true;
+            "bluez5.enable-msbc" = true;
+            "bluez5.enable-hw-volume" = true;
+            "bluez5.roles" = ["hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag"];
+          };
+        };
+      };
     };
 
     alsa.enable = true;
     alsa.support32Bit = true;
+    audio.enable = true;
     jack.enable = true;
     pulse.enable = true;
   };
