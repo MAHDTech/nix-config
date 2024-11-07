95efee56d18555efbc4b0862fbc24aab01cf2f0a
MAHDTech <MAHDTech@saltlabs.tech>
2024-11-08 10:28:40 +1100

    fix: wireplumber

--- nixos/system/config/bluetooth/default.nix
+++ nixos/system/config/bluetooth/default.nix
@@ -7,12 +7,23 @@
 
   hardware.bluetooth = {
     enable = true;
+    package = pkgs.bluez;
     powerOnBoot = true;
+    hsphfpd.enable = false;
+    # Bluez settings
+    # https://github.com/bluez/bluez/blob/3818b99c764efe84cd3455081f6392c256564085/src/main.conf
     settings = {
       General = {
+        ControllerMode = "dual";
         Enable = "Source,Sink,Media,Socket";
         Experimental = true;
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
       };
     };
+    disabledPlugins = [
+    ];
   };
 }
