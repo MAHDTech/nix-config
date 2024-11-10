8a6c537f2170059241565f968d4634f399c885d8
MAHDTech <MAHDTech@saltlabs.tech>
2024-11-11 10:25:03 +1100

    fix: TLP woes

--- nixos/system/soe/services/tlp/default.nix
+++ nixos/system/soe/services/tlp/default.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  # https://linrunner.de/tlp/settings/processor.html#cpu-energy-perf-policy-on-ac-bat
+  # https://linrunner.de/tlp/settings/processor.html
 
   imports = [];
 
@@ -14,11 +14,11 @@
       CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
 
-      CPU_DRIVER_OPMODE_ON_AC = "ondemand";
-      CPU_DRIVER_OPMODE_ON_BAT = "powersave";
+      CPU_DRIVER_OPMODE_ON_AC = "active";
+      CPU_DRIVER_OPMODE_ON_BAT = "passive";
 
-      CPU_SCALING_GOVERNOR_ON_AC = "ondemand";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "ondemand";
 
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
