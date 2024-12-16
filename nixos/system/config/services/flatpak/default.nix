adabd580b5181f461bf5cb7c7aa344dc0d4f7967
MAHDTech <MAHDTech@saltlabs.tech>
2024-12-16 14:32:35 +1100

    chore: Update flake.lock

--- nixos/system/config/services/flatpak/default.nix
+++ nixos/system/config/services/flatpak/default.nix
@@ -37,6 +37,7 @@
       "flathub:app/org.gimp.GIMP//stable"
       # "flathub:app/org.signal.Signal//stable" # Using Signal from nixpkgs instead.
       "flathub:app/org.videolan.VLC//stable"
+      "flathub:app/info.beyondallreason.bar//stable"
     ];
 
     remotes = {
