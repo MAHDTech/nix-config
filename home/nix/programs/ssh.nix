d6e88b121734e6f41eb0877fa13a209cefccb4b2
MAHDTech <MAHDTech@saltlabs.tech>
2024-12-24 15:59:31 +1100

    fix: ssh config

--- home/nix/programs/ssh.nix
+++ home/nix/programs/ssh.nix
@@ -62,16 +62,19 @@
     matchBlocks = {
       "github.com" = {
         host = "github.com";
+        hostname = "github.com";
         user = "git";
       };
 
       "gitlab.com" = {
         host = "gitlab.com";
+        hostname = "gitlab.com";
         user = "git";
       };
 
       "jumpbox-cloudforge" = {
-        host = "52.65.126.176";
+        host = "jumpbox-cloudforge";
+        hostname = "52.65.126.176";
         user = "dev_02";
       };
     };
