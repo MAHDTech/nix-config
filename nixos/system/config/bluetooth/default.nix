819a6ccfe720c42a9a1d221327da175e97614001
MAHDTech <MAHDTech@saltlabs.tech>
2025-05-05 09:33:01 +1000

    fix: bluetooth

--- nixos/system/config/bluetooth/default.nix
+++ nixos/system/config/bluetooth/default.nix
@@ -12,18 +12,19 @@
 
   hardware.bluetooth = {
     enable = true;
-    package = pkgs.bluez-experimental;
+    package = pkgs.bluez;
+    #package = pkgs.bluez-experimental;
     powerOnBoot = true;
     hsphfpd.enable = false; # conflicts with wireplumber
     # Bluez settings
     # https://github.com/bluez/bluez/blob/3818b99c764efe84cd3455081f6392c256564085/src/main.conf
     settings = {
       General = {
-        ControllerMode = "dual";
+        ControllerMode = "dual"; # Both BR/EDR and LE are enabled
         Enable = "Source,Sink,Media,Socket";
-        Experimental = "true";
+        Experimental = "false"; # Enable DBUS experimental interfaces
         FastConnectable = "true";
-        KernelExperimental = "true";
+        KernelExperimental = "false"; # Enables kernel experimental features via UUID.
       };
       Policy = {
         AutoEnable = "true";
