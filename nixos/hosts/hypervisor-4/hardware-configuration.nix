249b2c4de327a71f29313ce7c9b781d3e6c6a316
MAHDTech <MAHDTech@saltlabs.tech>
2025-07-12 09:49:33 +0000

    fix: whoops, wrong disk

--- nixos/hosts/hypervisor-4/hardware-configuration.nix
+++ nixos/hosts/hypervisor-4/hardware-configuration.nix
@@ -75,7 +75,7 @@
 
     # UEFI boot partition on USB
     "/boot/efi" = {
-      device = "/dev/disk/by-uuid/12CE-A600";
+      device = "/dev/disk/by-id/???-part1";
       fsType = "vfat";
       options = [
         "umask=0022"
