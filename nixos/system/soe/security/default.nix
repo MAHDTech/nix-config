bdef29eb2d709233424236dd43d0e57089540185
MAHDTech <MAHDTech@saltlabs.tech>
2025-07-14 10:56:40 +1000

    fix: breaking sudoers

--- nixos/system/soe/security/default.nix
+++ nixos/system/soe/security/default.nix
@@ -4,10 +4,7 @@
   ];
 
   security.sudo.configFile = ''
-    %nixos-admins  ALL=(ALL) NOPASSWD: /usr/bin/nixos-rebuild, /run/current-system/sw/bin/nixos-rebuild
-
-    %wheel  ALL=(ALL) NOPASSWD: ALL
-    %wheel  ALL=(ALL:ALL) NOPASSWD: SETENV: ALL
+    %nixos-admins  ALL=(ALL:ALL) NOPASSWD: SETENV: ALL
   '';
 
 }
