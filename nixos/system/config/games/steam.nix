a3f35597ed0c57a5f2312af8a6107852d439fd5d
MAHDTech <MAHDTech@saltlabs.tech>
2025-09-14 23:15:52 +1000

    feat: steam

--- nixos/system/config/games/steam.nix
+++ nixos/system/config/games/steam.nix
@@ -1,6 +1,25 @@
 {
 
-  # NOTE: Steam is installed via flatpak.
+  programs = {
+    steam = {
+      enable = true;
+      remotePlay = {
+        openFirewall = true;
+      };
+      protontricks = {
+        enable = true;
+      };
+      gamescopeSession = {
+        enable = true;
+      };
+      extest = {
+        enable = true;
+      };
+      dedicatedServer = {
+        openFirewall = true;
+      };
+    };
+  };
 
   # Allow ports required for Steam Link.
   networking = {
