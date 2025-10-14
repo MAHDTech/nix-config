1874a4d3c3b9045bdb946bc8e9c761c4b9a3cbeb
MAHDTech <MAHDTech@saltlabs.tech>
2025-10-15 07:31:13 +1100

    chore: update flake.lock

--- home/nix/packages/devenv.nix
+++ home/nix/packages/devenv.nix
@@ -1,6 +1,22 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    cachix
+{
+  inputs,
+  pkgs,
+  ...
+}:
+let
+  pkgsUnstable = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system};
+
+  unstablePkgs = with pkgsUnstable; [
     devenv
   ];
+in
+{
+
+  home.packages =
+    with pkgs;
+    [
+      cachix
+      #devenv
+    ]
+    ++ unstablePkgs;
 }
