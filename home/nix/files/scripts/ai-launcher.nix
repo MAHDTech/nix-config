3c0dcac0686f6e6d2e8069d541513b2be28c4edd
MAHDTech <MAHDTech@saltlabs.tech>
2026-03-17 22:41:38 +1100

    feat: ai launcher yaml config

--- home/nix/files/scripts/ai-launcher.nix
+++ home/nix/files/scripts/ai-launcher.nix
@@ -9,24 +9,32 @@ let
       huggingface-hub
       rich
       psutil
+      pyyaml
     ]
   );
 in
 {
-  # Make sure vulkan-tools is installed so Python can query your GPU VRAM!
-  home.packages = with pkgs; [
-    mesa-demos
-    vulkan-tools
-  ];
+  home = {
+    # Make sure vulkan-tools is installed so Python can query your GPU VRAM!
+    packages = with pkgs; [
+      mesa-demos
+      vulkan-tools
+    ];
 
-  home.file."ai-launcher" = {
-    target = "${config.home.homeDirectory}/.local/bin/ai-launcher";
-    executable = true;
+    file."ai-launcher" = {
+      target = "${config.home.homeDirectory}/.local/bin/ai-launcher";
+      executable = true;
 
-    # This dynamically injects the isolated Python path into your raw Python file
-    text = ''
-      #!${aiPython}/bin/python3
-    ''
-    + builtins.readFile ./src/ai-launcher.py;
+      # This dynamically injects the isolated Python path into your raw Python file
+      text = ''
+        #!${aiPython}/bin/python3
+      ''
+      + builtins.readFile ./src/ai-launcher.py;
+    };
+
+    file."ai-launcher-yaml" = {
+      target = "${config.home.homeDirectory}/.local/bin/ai-launcher.yaml";
+      source = ./src/ai-launcher.yaml;
+    };
   };
 }
