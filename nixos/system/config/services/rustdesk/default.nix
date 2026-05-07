f2520fb7a35e25aac5bd74835dbda5e74dd6b5e6
MAHDTech <MAHDTech@saltlabs.tech>
2026-05-08 09:16:53 +1000

    fix: rustdesk

--- nixos/system/config/services/rustdesk/default.nix
+++ nixos/system/config/services/rustdesk/default.nix
@@ -1,5 +1,11 @@
 { pkgs, ... }:
 {
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+  };
+
   environment.systemPackages = with pkgs; [
     rustdesk-flutter
   ];
