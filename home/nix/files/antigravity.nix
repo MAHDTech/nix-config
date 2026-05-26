e24a383b0d3f885b2a55ac064111f73528d24813
MAHDTech <MAHDTech@saltlabs.tech>
2026-05-27 00:55:23 +1000

    fix: antigravity settings

--- home/nix/files/antigravity.nix
+++ home/nix/files/antigravity.nix
@@ -1,8 +1,17 @@
-{ config, ... }:
+{
+  pkgs,
+  lib,
+  ...
+}:
 {
   home.file = {
-    "antigravity-cli-settings" = {
-      target = "${config.home.homeDirectory}/.gemini/antigravity-cli/settings.json";
+
+    #########################
+    # Antigravity CLI
+    #########################
+
+    "antigravity-cli-settings-tmpl" = {
+      target = ".gemini/antigravity-cli/settings.tmpl.json";
       executable = false;
 
       text = ''
@@ -22,8 +31,12 @@
       '';
     };
 
-    "antigravity-hub-config" = {
-      target = "${config.home.homeDirectory}/.gemini/config/config.json";
+    #########################
+    # Antigravity Hub
+    #########################
+
+    "antigravity-hub-config-tmpl" = {
+      target = ".gemini/config/config.tmpl.json";
       executable = false;
 
       text = ''
@@ -44,5 +57,45 @@
         }
       '';
     };
+  };
+
+  #########################
+  # Activation scripts
+  #########################
+
+  home.activation = {
+    mergeAntigravitySettings = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      # Merge CLI settings
+      CLI_TMPL="$HOME/.gemini/antigravity-cli/settings.tmpl.json"
+      CLI_TARGET="$HOME/.gemini/antigravity-cli/settings.json"
+      CLI_TMP_TARGET="$(mktemp)"
+
+      mkdir -p "$HOME/.gemini/antigravity-cli"
+
+      if [ -f "$CLI_TARGET" ];
+      then
+        ${pkgs.jq}/bin/jq -s '.[0] * .[1]' "$CLI_TARGET" "$CLI_TMPL" > "$CLI_TMP_TARGET"
+        mv --force "$CLI_TMP_TARGET" "$CLI_TARGET"
+      else
+        cp --force "$CLI_TMPL" "$CLI_TARGET"
+      fi
+      chmod 600 "$CLI_TARGET"
+
+      # Merge Hub config
+      HUB_TMPL="$HOME/.gemini/config/config.tmpl.json"
+      HUB_TARGET="$HOME/.gemini/config/config.json"
+      HUB_TMP_TARGET="$(mktemp)"
+
+      mkdir -p "$HOME/.gemini/config"
+
+      if [ -f "$HUB_TARGET" ];
+      then
+        ${pkgs.jq}/bin/jq -s '.[0] * .[1]' "$HUB_TARGET" "$HUB_TMPL" > "$HUB_TMP_TARGET"
+        mv --force "$HUB_TMP_TARGET" "$HUB_TARGET"
+      else
+        cp --force "$HUB_TMPL" "$HUB_TARGET"
+      fi
+      chmod 600 "$HUB_TARGET"
+    '';
   };
 }
