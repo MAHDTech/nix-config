9c32ca66b45d48e53e498d7e2e37ea9ff85c955c
MAHDTech <MAHDTech@saltlabs.tech>
2026-07-10 19:36:22 +1000

    fix: bambu and arm64 are not friends

--- home/nix/packages/custom/default.nix
+++ home/nix/packages/custom/default.nix
@@ -4,10 +4,15 @@
 }:
 let
   litert-lm = pkgs.callPackage ./litert-lm.nix { };
+
+  bambuStudio =
+    if pkgs.stdenv.hostPlatform.system == "x86_64-linux" then
+      [ (pkgs.callPackage ./BambuStudio.nix { }) ]
+    else
+      [ ];
 in
 {
   home.packages = [
-    (pkgs.callPackage ./BambuStudio.nix { })
     #(pkgs.callPackage ./OrcaSlicer.nix { })
 
     #(pkgs.callPackage ./cursor.nix {})
@@ -26,5 +31,6 @@ in
 
     #(pkgs.callPackage ./ls-colors.nix { })
 
-  ];
+  ]
+  ++ bambuStudio;
 }
