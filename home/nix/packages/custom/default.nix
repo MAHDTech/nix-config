ab86198075673b9cf188f2b935709ee672a8d233
MAHDTech <MAHDTech@saltlabs.tech>
2026-08-29 21:53:32 +1000

    chore: bump agy hub

--- home/nix/packages/custom/default.nix
+++ home/nix/packages/custom/default.nix
@@ -11,28 +11,18 @@ let
       [ ];
 in
 {
-  #home.packages = [
-  #(pkgs.callPackage ./OrcaSlicer.nix { })
-
-  #(pkgs.callPackage ./cursor.nix {})
-
-  #(pkgs.callPackage ./tkg.nix {})
-
-  #(pkgs.callPackage ./carvel.nix {})
-
-  #(pkgs.callPackage ./kpack.nix {})
-
-  #(pkgs.callPackage ./pivnet.nix {})
-
-  #(pkgs.callPackage ./litert-lm.nix {})
-
-  #(pkgs.callPackage ./antigravity-cli { })
-  #(pkgs.callPackage ./antigravity-hub { })
-
-  #(pkgs.callPackage ./claude-code { })
-
-  #(pkgs.callPackage ./ls-colors.nix { })
-
-  #]
-  home.packages = bambuStudio;
+  home.packages = [
+    #(pkgs.callPackage ./OrcaSlicer.nix { })
+    #(pkgs.callPackage ./cursor.nix {})
+    #(pkgs.callPackage ./tkg.nix {})
+    #(pkgs.callPackage ./carvel.nix {})
+    #(pkgs.callPackage ./kpack.nix {})
+    #(pkgs.callPackage ./pivnet.nix {})
+    #(pkgs.callPackage ./litert-lm.nix {})
+    #(pkgs.callPackage ./antigravity-cli { })
+    (pkgs.callPackage ./antigravity-hub { })
+    #(pkgs.callPackage ./claude-code { })
+    #(pkgs.callPackage ./ls-colors.nix { })
+  ]
+  ++ bambuStudio;
 }
